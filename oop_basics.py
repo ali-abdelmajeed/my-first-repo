f102a5a27c22dd8ed1c454b17be8b951b26a1478
Ali Abdelmajeed <ali.abdelmajeed.dev@gmail.com>
2026-04-16 20:28:08 +0200

    Add OOP exercises: BankAccounts

--- oop_basics.py
+++ oop_basics.py
@@ -19,6 +19,49 @@ class BankAccount:
     def display_info(self):
         print(f"Name: {self.owner} \n Balance: {self.balance} \n Account_number: {self.account_number}")
 
+class SavingAccount(BankAccount):
+    def __init__(self, owner, balance, account_number, interest_rate):
+        super().__init__(owner, balance, account_number)
+        self.interest_rate = interest_rate
+    
+    def add_interest(self):
+        interest = self.balance * self.interest_rate
+        self.balance += interest
+        print(f"Interest added {interest}, New balance {self.balance}")
+
+class PremiumAccount(BankAccount):
+    def __init__(self, owner, balance, account_number, credit_limit):
+        super().__init__(owner, balance, account_number)
+        self.credit_limit = credit_limit
+
+    def borrow(self, amount):
+        if amount > self.balance:
+            if amount <= self.credit_limit:
+                self.credit_limit -= amount
+                print(f"Borrowed {amount} from credit. Remaining credit: {self.credit_limit}")
+            else:
+                print("Can't borrow. Exceeds credit limit!")
+        else:
+            self.withdraw(amount)
+
+    def display_info(self):
+        super().display_info()
+        print(f"Credit Limit: {self.credit_limit}")
+        
+
+
+premium1 = PremiumAccount("Ali", 1000, "PRE-001", 5000)
+premium1.display_info()
+premium1.withdraw(500)
+premium1.borrow(2000)
+premium1.display_info()
+
+savings1 = SavingAccount("Ali", 1000, "SAV-001", 0.05)
+savings1.display_info()
+savings1.deposit(500)
+savings1.add_interest()
+savings1.withdraw(200)
+
 account1 = BankAccount("Ali", 1000, "ACC-001")
 account2 = BankAccount("Ahmed", 500, "ACC-002")
 
